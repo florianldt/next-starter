528c2414f7b85df88bf2ed773e012d7a8b79d2fc
florianldt <florian.ludot@gmail.com>
2020-09-19 13:14:10 +0200

    implement progress bar on page load

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import '../styles/globals.css';
+import 'nprogress/nprogress.css';
+
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const App = ({ Component, pageProps }) => <Component {...pageProps} />;
 
